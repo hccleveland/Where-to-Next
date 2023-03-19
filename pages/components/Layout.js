784819dc51645878c90daaa95e60981653fa445b
Kenneth Man <kenneth_man@icloud.com>
2023-03-19 21:33:54 +0900

    feat: integrated map component inside index

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,24 +1,34 @@
-import Footer from "./Footer"
-import Navbar from "./Navbar"
-import { createContext,useState } from 'react';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import { createContext, useState } from 'react';
 
 const AppContext = createContext();
 
 const Layout = ({ children }) => {
-  const [email, setEmail]=useState('');
-  const [airport, setAirport]=useState('');
-  const [display_name, setDisplay_name]=useState('');
-  const [first_name, setFirst_name]=useState('');
-  const [last_name, setLast_name]=useState('');
-  
+  const [email, setEmail] = useState('');
+  const [airport, setAirport] = useState('');
+  const [display_name, setDisplay_name] = useState('');
+  const [first_name, setFirst_name] = useState('');
+  const [last_name, setLast_name] = useState('');
+  const [currentPage, setCurrentPage] = useState('');
+
   return (
-    <AppContext.Provider value={{Email:[email,setEmail], Airport:[airport,setAirport], Display_name:[display_name, setDisplay_name], First_name:[first_name,setFirst_name], Last_name:[last_name,setLast_name]}}>
+    <AppContext.Provider
+      value={{
+        Email: [email, setEmail],
+        Airport: [airport, setAirport],
+        Display_name: [display_name, setDisplay_name],
+        First_name: [first_name, setFirst_name],
+        Last_name: [last_name, setLast_name],
+        Current_Page: [currentPage, setCurrentPage],
+      }}
+    >
       <Navbar />
-      { children }
+      {children}
       <Footer />
     </AppContext.Provider>
   );
-}
- 
+};
+
 export default Layout;
 export { AppContext };
--- pages/components/Message.js
+++ pages/components/Message.js
@@ -6,22 +6,20 @@ receive the props and display the messages related to that place */
 
 import React from 'react';
 
-export default function Message (props) { 
-    console.log(props.city);
-    console.log(props.example.city);
-    return ( <>
-            <div className='highlight'>
-                <div className='highlight-place' >
-                    {props.example.city}, {props.example.country}
-                    <br></br>
-                    {props.example.comment}
-                    <br></br>
-                    {props.example.display_name} {props.example.timestamp}
-                </div>
-            </div>   
-        </>
-    )
+export default function Message(props) {
+  //console.log(props.city);
+  //console.log(props.example.city);
+  return (
+    <>
+      <div className='highlight'>
+        <div className='highlight-place'>
+          {props.example.city}, {props.example.country}
+          <br></br>
+          {props.example.comment}
+          <br></br>
+          {props.example.display_name} {props.example.timestamp}
+        </div>
+      </div>
+    </>
+  );
 }
-
-
-
